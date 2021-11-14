84f52dc4fdfce78a6bcecc2851b82e74868df17c
Jonathan <jonathan@carter.games>
2021-11-14 19:01:05 +0000

    Patched Multi-Scene Asset
    - Added the multi scene patch and changed it to the release version
    - Fixed some minor issues with the project files for some reason

---
+++ Assets/_AssetPacks/Multi Scene Workflow/Extensions/Multi Scene URP Extension/MultiSceneBaseCamera.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * 
+ *  Multi-Scene Workflow
+ *      URP Camera Extension
+ *  
+ *	Multi-Scene Base Camera
+ *      Handles the base camera setup
+ *			
+ *  Written by:
+ *      Jonathan Carter
+ *		
+ *	Last Updated: 05/11/2021 (d/m/y)
+ * 
+ */
+
+
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace MultiScene.Extensions.URP
+{
+    [ExecuteInEditMode]
+    public class MultiSceneBaseCamera : MonoBehaviour
+    {
+        public string camID;
+        private Camera cam;
+
+        public Camera GetCamera => cam;
+
+        private void Awake()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        /// <summary>
+        /// Adds a camera to this base camera stack.
+        /// </summary>
+        /// <param name="overlayCam">The overlay camera to add to the stack.</param>
+        public void AddCamera(Camera overlayCam)
+        {
+            var _cameraData = cam.GetUniversalAdditionalCameraData();
+            _cameraData.cameraStack.Add(overlayCam);
+        }
+    }
+}
