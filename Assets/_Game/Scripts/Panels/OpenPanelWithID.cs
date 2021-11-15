47df905f827314190314d466aac17b9af9484d41
Jonathan <jonathan@carter.games>
2021-11-15 10:25:49 +0000

    Menu Setup
    - Added a font for the game
    - Basic layout of the main menu is complete
    - Added in some generic art assets for UI in the game
    - Fixed scene loading so the fade actually fades to clear when the scenes are loaded.

---
+++ Assets/_Game/Scripts/Panels/OpenPanelWithID.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MultiScene.Core;
+using UnityEngine;
+
+namespace DeadTired.UI
+{
+    public class OpenPanelWithID : MonoBehaviour, IMultiSceneAwake
+    {
+        private List<BasePanel> allPanels;
+
+
+        public void OnMultiSceneAwake()
+        {
+            allPanels = SceneElly.GetComponentsFromAllScenes<BasePanel>();
+        }
+
+
+        public void OpenPanel(string id)
+        {
+            allPanels.FirstOrDefault(t => t.PanelID.Equals(id))?.OpenPanel();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Panels/PlayButton.cs
@@ -0,0 +1,22 @@
+﻿using DeadTired.Scenes;
+using MultiScene.Core;
+using UnityEngine;
+
+namespace DeadTired.UI
+{
+    public class PlayButton : MonoBehaviour, IMultiSceneAwake
+    {
+        private SceneGroupChangeWithFade levelLoad;
+
+        public void OnMultiSceneAwake()
+        {
+            levelLoad = SceneElly.GetComponentFromScene<SceneGroupChangeWithFade>("Menu");
+        }
+
+        public void PlayGame()
+        {
+            levelLoad.LevelSelected = Levels.Level1;
+            levelLoad.LoadSceneGroup();
+        }
+    }
+}
