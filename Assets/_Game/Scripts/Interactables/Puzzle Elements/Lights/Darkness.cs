e337bfc1900757542a8eccc3497e265121cd2b1e
Jonathan <jonathan@carter.games>
2021-11-16 13:20:44 +0000

    Level 1 Completable
    - Level 1 is now possible to complete.
    - Just need to add a death / reset mechanic for the darkness and I'll get to work on level 2.

---
+++ Assets/_Game/Scripts/Interactables/Puzzle Elements/Lights/Darkness.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace DeadTired.Interactables
+{
+    public class Darkness : MonoBehaviour
+    {
+        [SerializeField] private Transform dragSpot;
+        private BoxCollider triggerZone;
+
+
+        private void Awake()
+        {
+            triggerZone = GetComponent<BoxCollider>();
+        }
+
+
+        private void ToggleDarkness()
+        {
+            triggerZone.enabled = !triggerZone.enabled;
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+         
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+     
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+       
+        }
+
+
+        private void KillPlayer()
+        {
+            
+        }
+    }
+}
