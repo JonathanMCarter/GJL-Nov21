fa02e60f1393961e29ac47258d096b518387f1b5
Jonathan <jonathan@carter.games>
2021-11-16 14:24:27 +0000

    Added Death Mechanics

--- Assets/_Game/Scripts/Interactables/Puzzle Elements/Lights/Darkness.cs
+++ Assets/_Game/Scripts/Interactables/Puzzle Elements/Lights/Darkness.cs
@@ -1,47 +1,50 @@
-﻿using System;
+﻿using DeadTired.Death;
+using DependencyLibrary;
 using UnityEngine;
 
 namespace DeadTired.Interactables
 {
     public class Darkness : MonoBehaviour
     {
-        [SerializeField] private Transform dragSpot;
+        [SerializeField] private BoolReference isPlayerGhost;
         private BoxCollider triggerZone;
+        private bool calledToKill;
 
 
         private void Awake()
         {
+            if (triggerZone != null) return;
             triggerZone = GetComponent<BoxCollider>();
         }
-
-
-        private void ToggleDarkness()
+        
+        public void ToggleDarkness(bool value)
         {
-            triggerZone.enabled = !triggerZone.enabled;
+            if (triggerZone == null)
+                triggerZone = GetComponent<BoxCollider>();
+            
+            triggerZone.enabled = value;
         }
         
         private void OnTriggerEnter(Collider other)
         {
+            if (isPlayerGhost.Value) return;
             if (!other.CompareTag("Player")) return;
-         
+            if (calledToKill) return;
+            KillPlayer();
         }
 
         private void OnTriggerStay(Collider other)
         {
+            if (isPlayerGhost.Value) return;
             if (!other.CompareTag("Player")) return;
-     
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (!other.CompareTag("Player")) return;
-       
+            if (calledToKill) return;
+            KillPlayer();
         }
 
 
         private void KillPlayer()
         {
-            
+            PlayerDeathController.OnPlayedKilled?.Invoke();
         }
     }
 }
