b95af791396f420f53a9db8cddbaf738907f8130
stathampen <33280323+stathampen@users.noreply.github.com>
2021-11-18 15:31:31 +0000

    Setting to look through walls  when player is behind them

--- Assets/_Game/Scripts/UI Displays/CutOutSync.cs
+++ Assets/_Game/Scripts/UI Displays/CutOutSync.cs
@@ -6,16 +6,39 @@ namespace DeadTired
 {
     public class CutOutSync : MonoBehaviour
     {
-        // Start is called before the first frame update
+
+        public static int posID = Shader.PropertyToID("_Positon");
+        public static int sizeID = Shader.PropertyToID("_Size");
+
+        public Material wallMaterial;
+        public Camera camera;
+        public LayerMask mask;
+
         void Start()
         {
-        
+            if(camera == null)
+                camera = Camera.main;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
+            //check if behind or infront of a wall
+            var dir = camera.transform.position - transform.position;
+            var ray = new Ray(transform.position, dir.normalized);
+
+            if(Physics.Raycast(ray, 3000, mask))
+            {
+                wallMaterial.SetFloat(sizeID, 1);
+            }
+            else
+            {
+                wallMaterial.SetFloat(sizeID, 0);
+            }
+
+            var view = camera.WorldToViewportPoint(transform.position);
+            wallMaterial.SetVector(posID, view);
         }
     }
 }
